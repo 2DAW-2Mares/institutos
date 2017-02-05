a0aac3111c1761cdf36e75c7a0c0d275fb0655bd
Alberto Sierra <alberto.sierra@murciaeduca.es>
2017-02-05 21:47:12 +0100

    Environment-specific configuration

--- server/boot/automigrateDB.js
+++ server/boot/automigrateDB.js
@@ -1,18 +1,15 @@
 module.exports = function(app) {
-  app.dataSources.db.automigrate('User', function(err) {if (err) throw err;});
-  app.dataSources.db.automigrate('AccessToken', function(err) {if (err) throw err;});
-  app.dataSources.db.automigrate('ACL', function(err) {if (err) throw err;});
-  app.dataSources.db.automigrate('RoleMapping', function(err) {if (err) throw err;});
-  app.dataSources.db.automigrate('Role', function(err) {if (err) throw err;});
-  app.dataSources.db.automigrate('Anyoescolar', function(err) {if (err) throw err;});
-  app.dataSources.db.automigrate('Centro', function(err) {if (err) throw err;});
-  app.dataSources.db.automigrate('Grupo', function(err) {if (err) throw err;});
-  app.dataSources.db.automigrate('Materia', function(err) {if (err) throw err;});
-  app.dataSources.db.automigrate('Materiaimpartida', function(err) {if (err) throw err;});
-  app.dataSources.db.automigrate('Materiamatriculada', function(err) {if (err) throw err;});
-  app.dataSources.db.automigrate('Matricula', function(err) {if (err) throw err;});
-  app.dataSources.db.automigrate('Nivel', function(err) {if (err) throw err;});
-  app.dataSources.db.automigrate('Usuario', function(err) {if (err) throw err;});
-  app.dataSources.db.automigrate('Tutorizado', function(err) {if (err) throw err;});
-};
 
+  var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+  app.dataSources.db.automigrate(lbTables, function(er) {
+    if (er) throw er;
+    console.log('Loopback tables [' , lbTables , '] created in ', app.dataSources.db.adapter.name);
+  });
+
+  var institutosTables = ['Anyoescolar', 'Centro', 'Grupo', 'Materia', 'Materiaimpartida', 'Materiamatriculada', 'Matricula', 'Nivel', 'Usuario', 'Tutorizado'];
+  app.dataSources.db.automigrate(institutosTables, function(er) {
+    if (er) throw er;
+    console.log('Loopback tables [' , institutosTables , '] created in ', app.dataSources.db.adapter.name);
+  });
+
+};
