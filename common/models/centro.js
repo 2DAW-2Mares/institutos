1eb4a641853c7c2c66d34e0860bbd9d976132134
Alberto Sierra <alberto.sierra@murciaeduca.es>
2017-02-12 19:24:35 +0100

    Asociando el usuario autenticado al centro de nueva creación

--- common/models/centro.js
+++ common/models/centro.js
@@ -5,6 +5,7 @@ module.exports = function(Centro) {
 		if (ctx.isNewInstance) {
 			if (ctx.instance) {
 				ctx.instance.unsetAttribute('verificado');
+				ctx.instance.coordinador = ctx.options && ctx.options.accessToken && ctx.options.accessToken.userId;
 			} else {
 				delete ctx.data.verificado;
 			}
