bc76bc9b2bc48909e5db60e892e5fa5a85a728c8
Alberto Sierra <alberto.sierra@murciaeduca.es>
2017-02-12 12:30:55 +0100

    Centralizando la configuración en config.local.js

--- server/datasources.staging.js
+++ server/datasources.staging.js
@@ -3,32 +3,9 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
+var config = require('./config.local')
+
 module.exports = {
-  db: {
-    connector: 'mysql',
-    hostname: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: 'instituto',
-  },
-  emailDs: {
-    name: 'emailDs',
-    connector: 'mail',
-    transports: [
-      {
-        type: 'smtp',
-        host: 'smtp.gmail.com',
-        secure: true,
-        port: 465,
-        tls: {
-          rejectUnauthorized: false
-        },
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASSWORD
-        }
-      }
-    ]
-  }
+	db: config.db,
+	emailDs: config.emailDs
 };
