341446e83b7aed25d493beb5a3217c0c1b78b64c
angel2433282 <billyjohe.com>
2017-02-17 12:09:47 +0100

    Permisos POST anyoescolar Coordinador

---
+++ server/boot/profesorRole.js
@@ -0,0 +1,31 @@
+module.exports = function(app) {
+  var Role = app.models.Role;
+
+  Role.registerResolver('profesor', function(role, context, cb) {
+
+    //Q: Is the user logged in? (there will be an accessToken with an ID if so)
+    var userId = context.accessToken.userId;
+    if (!userId) {
+      //A: No, user is NOT logged in: callback with FALSE
+      return process.nextTick(() => cb(null, false));
+    }
+    // (using count() because we only want to know if such a record exists)
+    var Materiaimpartida = app.models.Materiaimpartida;
+    Materiaimpartida.count({
+      userId: userId
+    }, function(err, count) {
+      // A: The datastore produced an error! Pass error to callback
+      if (err) return cb(err);
+
+      if(count > 0){
+        // A: YES. At least one Materiaimpartida associated with this User AND Project
+        return cb(null, true);
+      }
+	    else{
+        // A: NO, User is not in this Project's Materiaimpartida
+        return cb(null, false);
+      }
+    });
+  });
+};
+
