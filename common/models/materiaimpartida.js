1305b1e5d5b6ed060687dd74fdbc301fde029343
joycon <fika_sanjavier@hotmail.com>
2017-02-19 14:17:13 +0100

    Revert "Enviar un email al creador de un grupo cuando alguien haya creado una materia impartida para el grupo"
    This reverts commit 107b2fbefcda23e896b3171db56ee6ecfa81257c.

--- common/models/materiaimpartida.js
+++ common/models/materiaimpartida.js
@@ -1,31 +1,5 @@
 'use strict';
 
 module.exports = function(Materiaimpartida) {
-    Materiaimpartida.afterRemote('create', function(context, materiaimpartida, next) {
-		console.log('> materiaimpartida.afterRemote triggered');
-            
-        // modelos relacionados
-        materiaimpartida.materiaId(function(err, materia) {
-			if (err) throw err;
-			materiaimpartida.grupoId(function(err, grupo) {
-				if (err) throw err;
-				grupo.userId(function(err, creador) {
-					if (err) throw err;
 
-					// envio de correo al creador del grupo
-					var html = 'Nueva materia <strong>"'+materia.nombre+'"</strong>, asignada al grupo <strong>"'+grupo.nombre+'"</strong>';
-                    Materiaimpartida.app.models.Email.send({
-                        to: creador.email,
-                        from: config.emailDs.transports[0].auth.user,
-                        subject: 'Nueva materia asignada',
-                        html: html
-                    }, function(err) {
-                        if (err) return console.log('> error al enviar el email');
-                        console.log('> se ha enviado correctamente');
-                        next(); // evita enviar correos multiples
-                    });
-				})
-			})
-		})
-	});
 };
