497aa16ff24df3e71b301c67734a8e11413dd70e
Alumno Servidor <you@example.com>
2017-02-21 08:12:08 +0100

    POST Matriculas

--- common/models/matricula.js
+++ common/models/matricula.js
@@ -1,5 +1,28 @@
 'use strict';
+var app = require('../../server/server.js');
 
 module.exports = function(Matricula) {
 
+	Matricula.observe('before save', function removeUnwantedField(ctx, next) {
+		var Grupo = app.models.Grupo;
+
+		if (ctx.instance && ctx.instance.grupo) {
+			Grupo.findById(ctx.instance.grupo, function(err, grupo) {
+				if (err) {
+					var err = new Error('No existe ningún grupo con ese id');
+					err.statusCode = 404;
+					throw err;
+				} else {
+					if (grupo.verificado) {
+						console.log('Centro verificado correctamente');
+					} else {
+						var err = new Error('El centro no está verificado');
+						err.statusCode = 404;
+						throw err;
+					}
+				}
+			});
+		}
+		next();
+	});
 };
