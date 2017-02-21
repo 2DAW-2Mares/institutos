f435f8f70c50121312f6af8c9604ac5aedd77760
Alumno Servidor <you@example.com>
2017-02-21 08:21:17 +0100

    Peticion POST Matriculas

--- common/models/matricula.js
+++ common/models/matricula.js
@@ -1,12 +1,14 @@
 'use strict';
+// Importar la app
 var app = require('../../server/server.js');
 
 module.exports = function(Matricula) {
-
+	// Disparador que se ejecuta antes de guardar el registro
 	Matricula.observe('before save', function removeUnwantedField(ctx, next) {
+		// Importar el modelo Grupo
 		var Grupo = app.models.Grupo;
 
-		if (ctx.instance && ctx.instance.grupo) {
+		if (ctx.instance && ctx.instance.grupo) { // Contiene los datos de la petición POST
 			Grupo.findById(ctx.instance.grupo, function(err, grupo) {
 				if (err) {
 					var err = new Error('No existe ningún grupo con ese id');
