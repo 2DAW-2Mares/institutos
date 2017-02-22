af19a619d8be2228337a50502c44ce898745c22c
Alumno Servidor <you@example.com>
2017-02-22 10:45:02 +0100

    Peticion POST Matriculas Corregido

--- common/models/matricula.js
+++ common/models/matricula.js
@@ -5,26 +5,27 @@ var app = require('../../server/server.js');
 module.exports = function(Matricula) {
 	// Disparador que se ejecuta antes de guardar el registro
 	Matricula.observe('before save', function removeUnwantedField(ctx, next) {
-		// Importar el modelo Grupo
-		var Grupo = app.models.Grupo;
+		var Grupo = app.models.Grupo; // Importar el modelo Grupo
 
 		if (ctx.instance && ctx.instance.grupo) { // Contiene los datos de la petición POST
 			Grupo.findById(ctx.instance.grupo, function(err, grupo) {
 				if (err) {
 					var err = new Error('No existe ningún grupo con ese id');
 					err.statusCode = 404;
-					throw err;
+					console.log(err);
+					next(err); // Pasa al siguiente middleware de error
 				} else {
-					if (grupo.verificado) {
-						console.log('Centro verificado correctamente');
+					if (grupo && grupo.verificado) {
+						console.log('El centro está verificado correctamente');
+						next(); // Pasa al siguiente middleware
 					} else {
-						var err = new Error('El centro no está verificado');
+						var err = new Error('El centro no está verificado o no existe');
 						err.statusCode = 404;
-						throw err;
+						console.log(err);
+						next(err);
 					}
 				}
 			});
 		}
-		next();
 	});
 };
