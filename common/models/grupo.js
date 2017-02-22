9bb0dea346c9996ae7de63e65712d0f1ce574697
AlbertoVil <berxini@hotmail.com>
2017-02-22 11:04:10 +0100

    Correo a coordinador cuando se crea grupo asociado a un centro

--- common/models/grupo.js
+++ common/models/grupo.js
@@ -1,5 +1,44 @@
 'use strict';
 
+var config = require('../../server/config.local.js');
+var path = require('path');
+var app = require('../../server/server.js');
 module.exports = function(Grupo) {
 
+	Grupo.afterRemote('create', function correoCoordinador(context,grupo,next) {
+		console.log('> materiaimpartida.afterRemote triggered');
+		console.log(grupo);
+		grupo.anyoescolarId(function(err, anyoescolar) {
+			if (err) next(err);
+			anyoescolar.centroId(function(err, centro) {
+				console.log(centro);
+				if (err) next(err);
+				centro.userId(function(err, coordinador) {
+					console.log(coordinador);
+					if (err) next(err);
+
+
+					
+							console.log("Usuario Coordinador: " +coordinador.nombre + " Email: " + coordinador.email);
+							
+
+					var html = 'Nuevo grupo <strong>"'+grupo.nombre+'"</strong>, asignada al centro <strong>"'+centro.nombre+'"</strong>';
+                    Grupo.app.models.Email.send({
+                        to: coordinador.email,
+                        from: config.admin.email,
+                        subject: 'Nuevo grupo asignado al centro',
+                        html: html
+                    }, function(err) {
+                        if (err) return console.log('> error al enviar el email');
+                        console.log('> se ha enviado correctamente');
+                        next(); // evita enviar correos multiples
+                    });
+					
+				})
+
+			})
+
+		})
+	});
 };
+
