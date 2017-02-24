8b9aeabc41b092c5895f74e7435b7ab7ae7c9430
Alberto Sierra <alberto.sierra@murciaeduca.es>
2017-02-24 19:19:31 +0100

    Seleccionar automáticamente el valor del atributo aprobado

--- common/models/materiamatriculada.js
+++ common/models/materiamatriculada.js
@@ -2,6 +2,19 @@
 
 module.exports = function(Materiamatriculada) {
 
+	Materiamatriculada.observe('before save', function (ctx, next) {console.log(ctx);
+		if (ctx.data && ctx.data.calificacion) {
+			if (ctx.data.calificacion >= 5) {
+				ctx.data.aprobado = true;
+			} else {
+				ctx.data.aprobado = false;
+			}
+		}
+		next();
+	});
+
+
+
 	Materiamatriculada.califica = function(id, calificacion, cb) {
 		Materiamatriculada.findById(1, function(err, materiamatriculada) {
 			if (err) {
